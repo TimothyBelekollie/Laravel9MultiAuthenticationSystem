693d9e28d0c571ae2ca20246f3dacdc931600129
belekollietimothy2@gmail.com <belekollietimothy2@gmail.com>
2022-04-11 15:28:50 +0200

    latest update

--- Laravel9MulAuth/routes/web.php
+++ Laravel9MulAuth/routes/web.php
@@ -17,10 +17,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
+Route::middleware('admin:admin')->group(function (){
+    Roue::get('admin/login',[AdminController::class,'LoginForm']);
+     Roue::post('admin/login',[AdminController::class,'LoginForm'])->name('admin.login');
+});
+
+Route::middleware([ 'auth:sanctum,admin',config('jetstream.auth_session'), 'verified'
+])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+
+Route::middleware([ 'auth:sanctum',config('jetstream.auth_session'), 'verified'
 ])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
